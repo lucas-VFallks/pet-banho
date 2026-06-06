db6588c0ac4fbd17f4d3dd5afb1e37a52c5e2582
Lucas.dev <lucas.dev2007@gmail.com>
2026-06-06 20:18:55 -0300

    Refactor Pet class and remove redundant pet class

--- src/Pet.java
+++ src/Pet.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+public class Pet {
+    private final String name;
+    private boolean clean;
 
-public class pet {
+
+    public Pet(String name) {
+        this.name = name;
+        this.clean = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isClean() {
+        return clean;
+    }
+
+    public void setClean(boolean clean) {
+        this.clean = clean;
+    }
+    //class pet
 }
